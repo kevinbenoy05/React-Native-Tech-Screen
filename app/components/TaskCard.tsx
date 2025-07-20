ce88e1c1ab4bfcdcc6c76a81fa0fef632e2423f0
Kevin Benoy <kevinbenoy@gmail.com>
2025-07-20 11:11:42 +0530

    core functionality done

--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { Task } from '../../types';
 
-const taskItem = () => {
-  
+interface taskProps{ //Props object to pass in component
+  task: Task;
+  complete: (id:string) => void;
+  handleDelete: (id:string) => void;
+}
+const TaskCard = ({task, complete, handleDelete}: taskProps) => { //Task card
+  let status = task.complete? "complete":"incomplete"
   return (
     <View>
-      <Text>taskItem</Text>
+      <Text>ID: {task.id}</Text>
+      <Text>DESC: {task.desc}</Text>
+    
+      <Text>Status: {status}</Text>
+      <TouchableOpacity onPress={() =>complete(task.id)}>
+        <Text>Complete Task</Text>
+      </TouchableOpacity>
+      <TouchableOpacity  onPress={() =>handleDelete(task.id)}>
+        <Text>Delete Task</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-export default taskItem
+export default TaskCard
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { Task } from "./types";
+import { Task } from "../types";
+import TaskCard from "./components/TaskCard";
 
 
 export default function Index() {
@@ -15,17 +16,36 @@ export default function Index() {
     setTasks([...tasks, newTask])
     setQuery("")
   }
+  function markComplete(completeID:string) {
+  setTasks(
+    tasks.map((task)=>
+      task.id === completeID? {...task, complete: true}:{...task}
+    )
+  );
+};
+function deleteTask(delID:string){
+  let newTasks = tasks.filter(task => task.id !== delID)
+  setTasks([...newTasks])
+}
   return (
-    <View>
+    <View style ={{paddingTop:50}} className="flex-1">
       <TextInput 
+        className="h-10 border"
         placeholder='Enter new task...'
+        placeholderTextColor='#6b7280'
         value={query} 
         onChangeText={setQuery}></TextInput>
-      <TouchableOpacity 
+      <TouchableOpacity
+        className="mt-2"
         onPress={addTask}
       >
         <Text>Add task</Text>
       </TouchableOpacity>
+      {tasks.map((taskElement) => (
+        <TaskCard key = {taskElement.id} task = {taskElement} 
+        complete = {markComplete} 
+        handleDelete = {deleteTask}></TaskCard>
+      ))}
     </View>
     
   );
